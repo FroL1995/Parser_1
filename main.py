a7fc18d18f43ff1f6ec3103395b6372b503ec5aa
FroL1995 <137566026+FroL1995@users.noreply.github.com>
2024-11-29 18:26:44 +0000

    some changes

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = 'https://tl.rulate.ru/book/16292/327323/ready_new'
+    url = 'https://snaryazhenie-patronov.ru/russia/korolev/'
     response = requests.get(url)
     print(response.status_code)
-
+    # print(response.text)
+    
     soup = BeautifulSoup(response.text, 'html.parser')
-    part = soup.find("div", class_="text-container")
-    with open("page.txt", 'a', encoding='UTF-8') as file:
-        file.write(part.text)
+    spoon = soup.find_all("div", class_="product-block product-thumb")
+    print(spoon)
+
+    # part = soup.find("div", class_="text-container")
+    # with open("page.txt", 'a', encoding='UTF-8') as file:
+    #     file.write(part.text)
 
 
 if __name__ == '__main__':
